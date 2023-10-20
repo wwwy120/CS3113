47f9b5be1dc2441b142718457166ec2e3f05b555
Ariel Wang <arielwang@arieldembp.mynetworksettings.com>
2023-10-20 13:59:14 -0400

    Updated Spirite Path

--- HW2-Pong/SDLProject/main.cpp
+++ HW2-Pong/SDLProject/main.cpp
@@ -44,14 +44,14 @@ const int VIEWPORT_X      = 0,
 const float MILLISECONDS_IN_SECOND = 1000.0;
 
 //texture picture
-const char BALL_TEXTURE[] = "/Users/arielwang/Desktop/HW2-Pong/assets/art/flower.png";
-const char PLAYER_TEXTURE[] = "/Users/arielwang/Desktop/HW2-Pong/assets/art/player.png";
-const char TEXT_TEXTURE1[] = "/Users/arielwang/Desktop/HW2-Pong/assets/art/player1win.png";
-const char TEXT_TEXTURE2[] = "/Users/arielwang/Desktop/HW2-Pong/assets/art/player2win.png";
-//const char BALL_TEXTURE[] = "art/flower.png";
-//const char PLAYER_TEXTURE[] = "player.png";
-//const char TEXT_TEXTURE1[] = "player1win.png";
-//const char TEXT_TEXTURE2[] = "player2win.png";
+//const char BALL_TEXTURE[] = "/Users/arielwang/Desktop/HW2-Pong/assets/art/flower.png";
+//const char PLAYER_TEXTURE[] = "/Users/arielwang/Desktop/HW2-Pong/assets/art/player.png";
+//const char TEXT_TEXTURE1[] = "/Users/arielwang/Desktop/HW2-Pong/assets/art/player1win.png";
+//const char TEXT_TEXTURE2[] = "/Users/arielwang/Desktop/HW2-Pong/assets/art/player2win.png";
+const char BALL_TEXTURE[] = "art/flower.png";
+const char PLAYER_TEXTURE[] = "player.png";
+const char TEXT_TEXTURE1[] = "player1win.png";
+const char TEXT_TEXTURE2[] = "player2win.png";
 
 //------------------------------Shader Path------------------------------
 const char V_SHADER_PATH[] = "shaders/vertex_textured.glsl",
