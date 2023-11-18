2625f11d0e9b08b9fa14621ef39b6c67d6764eb7
Ariel Wang <arielwang@arieldembp.mynetworksettings.com>
2023-11-18 17:12:44 -0500

    Add header comment block

--- HW4-Rise of the AI/SDLProject/main.cpp
+++ HW4-Rise of the AI/SDLProject/main.cpp
@@ -1,3 +1,13 @@
+/**
+* Author: Ariel Wang
+* Assignment: Rise of the AI
+* Date due: 2023-11-18, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
+
 #define LOG(argument) std::cout << argument << '\n'
 #define STB_IMAGE_IMPLEMENTATION
 #define GL_SILENCE_DEPRECATION
